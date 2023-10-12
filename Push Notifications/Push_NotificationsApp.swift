a8fdd744fefdfe5b4423202e5ff36aa3db9beb9a
macpadilla37 <89860555+macpadilla37@users.noreply.github.com>
2023-10-11 19:15:04 -0600

    Initial Commit

---
+++ Push Notifications/Push_NotificationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Push_NotificationsApp.swift
+//  Push Notifications
+//
+//  Created by Mac Padilla on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Push_NotificationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
